dde11a35a6e94b8c7c3d769507e1c8d51edb1e0e
jiang_wj <464410531@163.com>
2019-11-29 11:24:22 +0800

    修改

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -11,7 +11,7 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <span v-if="name">{{ name || 未登录 }}</span>
+          <span v-if="name">{{ name || '未登录' }}</span>
           <img v-else :src="avatar + '?imageView2/1/w/80/h/80'" class="user-avatar" />
           <i class="el-icon-caret-bottom" />
         </div>
